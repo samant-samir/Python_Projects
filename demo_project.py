e6a0e356db703fb3ceb155ee9bfb17c9acde5627
Samir Samant <samant.samir@gmail.com>
2018-08-07 01:45:30 +0530

    first commit

--- demo_project.py
+++ demo_project.py
@@ -5,7 +5,7 @@ import json
 response = requests.get("https://jsonplaceholder.typicode.com/users")
 users = json.loads(response.text)
 
-def flattenjson( b, delim ):
+def flattenjson(b, delim):
     val = {}
     for i in b.keys():
         if isinstance( b[i], dict ):
@@ -14,13 +14,12 @@ def flattenjson( b, delim ):
                 val[ i + delim + j ] = get[j]
         else:
             val[i] = b[i]
-
     return val
 
 
 users = list(map( lambda x: flattenjson( x, "_" ), users ))
 columns = [ x for row in users for x in row.keys() ]
-columns = list( columns )
+columns = list(columns)
 
 print(json.dumps(users, indent=2))
 
